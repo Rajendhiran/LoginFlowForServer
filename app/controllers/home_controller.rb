8bf56d4659cc9e27c8110670b9bc5c301a093780
Ream <singachea@gmail.com>
2015-04-08 12:28:00 +0800

    add home controller with index, protected, unprotected pages

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:protected_page]
+
+  def index
+  end
+
+  def protected_page
+  end
+
+  def unprotected_page
+  end
+end
